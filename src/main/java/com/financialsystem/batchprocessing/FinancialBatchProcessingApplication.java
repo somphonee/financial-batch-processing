9749edc2fe33926b27a0c8faa1f162f2ecf6c552
spDev <sp.xayyalath@gmail.com>
2025-03-13 23:10:19 +0700

    [ADD] BatchConfig

--- src/main/java/com/financialsystem/batchprocessing/FinancialBatchProcessingApplication.java
+++ src/main/java/com/financialsystem/batchprocessing/FinancialBatchProcessingApplication.java
@@ -1,9 +1,13 @@
 package com.financialsystem.batchprocessing;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableBatchProcessing
+@EnableScheduling
 public class FinancialBatchProcessingApplication {
 
 	public static void main(String[] args) {
